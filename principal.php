bc8a1a2ae3d72e38833399ae72af365ff6ef007b
samanta <daybeldiaz14@gmail.com>
2022-07-26 11:20:43 -0400

    Menu y pie agregados
    Se agrego un menu desplegable y adaptable y un pie

--- principal.php
+++ principal.php
@@ -1,35 +1,63 @@
-<?php
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="3valtech inc.">
+    <title>3valtech</title>
+    <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/index.css" rel="stylesheet">
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+    <link rel="manifest" href="favicon/site.webmanifest">
+    <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="theme-color" content="#ffffff">
+</head>
+    <body>
+   <nav class="navbar navbar-expand-sm fixed-top navbar-ligth bg-ligth" aria-label="Third navbar example">
+    <div class="container-fluid">
+    <img src="images/logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      <a class="navbar-brand" href="#">3valtech</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample03" aria-controls="navbarsExample03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-$error;
-	
-	//Conseguir mensaje en INDEX.PHP
-	//if(!empty($_POST['user']) && !empty($_POST['pass'])) {
-		//$usuario = $_POST['user'];
-		//$clave = $_POST['pass'];
-		//if ($usuario == 'user1' && $clave == '1234'){
-		//	$error = 'OK';
-		//	echo 'Bienvenido al sistema';
-		//} else {
-		//	$error = 'incorrecto';
-		//	header('location: index.php?error=$error');
-		//}
-	//} else {
-	//	$error = 'vacio';
-	//	header('location: index.php?error=$error');
-	//}
+      <div class="collapse navbar-collapse" id="navbarsExample03">
+        <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled">Disabled</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<div class="container">
+  <footer class="py-3 my-4">
+    <p class="text-center text-muted">&copy; 2022 3valtech Inc.</p>
+  </footer>
+</div>
 
 
-$user = "Usuario1";
-$pass = "12345";
+<script src="vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 
-
-$username = $_POST['user'];
-$password = $_POST['pass'];
-
-	if ($user === $username && $pass === $password){
-		session_start();
-		echo 'Bienvenido al sistema';
-	}else{
-
-		echo 'Los datos son incorrectos o estan vacios';
-	}
+    </body>
+</html>
